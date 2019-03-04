df54f9ad25b7ddedd5ef289a6bc3d454ac43516b
chrissunny94 <chrissunny94@gmail.com>
2019-03-04 22:38:24 +0530

    first push

---
+++ README.txt
@@ -0,0 +1,4 @@
+This code is an example of how to run OpenCV Code inside of a ROS node.
+
+Basically, you create a standard ROS node and then just write some OpenCV
+code inside of it and it'll do what you say.
---
+++ src/opencv_test.cpp
@@ -0,0 +1,161 @@
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <laneDetection.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <string>
+#include <calibration.h>
+
+
+using namespace cv;
+using namespace std;
+
+#include <image_transport/image_transport.h>
+
+
+Mat videoFrame; // Video Frame.
+Mat videoFrameUndistorted; // Video Frame (after calibration).
+Mat videoFramePerspective; // Video Frame (after perspective transform).
+Mat _videoFrameUndistorted;
+Mat debugWindow(540, 1280, CV_8UC3, Scalar(0,0,0)); //The Debug window.
+Size videoSize; // Input Variable Size.
+Mat cameraMatrix, dist; //Calibration Matrix.
+Mat perspectiveMatrix; //Homography Matrix.
+Mat finalResult;
+String coordinatetext = "";
+Point2f perspectiveSrc[] = {Point2f(565,470), Point2f(721,470), Point2f(277,698), Point2f(1142,698)};
+Point2f perspectiveDst[] = {Point2f(300,0), Point2f(980,0), Point2f(300,720), Point2f(980,720)};
+stringstream ss;
+float laneDistant = 0;
+
+
+class OpenCVWebCam {
+	public:
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub;
+    image_transport::Publisher pub;
+    bool image_captured;
+    laneDetection LaneAlgo;
+
+    //This variable stores the current frame
+
+
+    OpenCVWebCam() :it_(nh_) {
+
+
+        image_captured = false;
+
+        //Get the Perspective Matrix.
+        perspectiveMatrix = getPerspectiveTransform(perspectiveSrc,perspectiveDst);
+
+        //--------------Camera Calibration Start-----------------
+        FileStorage fsRead;
+        fsRead.open("Intrinsic.xml", FileStorage::READ);
+        Mat src = imread("./camera_cal/calibration2.jpg");
+        Mat dst;
+        if (fsRead.isOpened() == false)
+        {
+            CameraCalibrator myCameraCalibrator;
+            myCameraCalibrator.doCalibration(cameraMatrix, dist);
+            FileStorage fs;
+            fs.open("Intrinsic.xml", FileStorage::WRITE);
+            fs << "CameraMatrix" << cameraMatrix;
+            fs << "Dist" << dist;
+            fs.release();
+            fsRead.release();
+            cout << "There is no existing intrinsic parameters XML file." << endl;
+            cout << "Start calibraton......" << endl;
+        }
+        else
+        {
+            fsRead["CameraMatrix"] >> cameraMatrix;
+            fsRead["Dist"] >> dist;
+            fsRead.release();
+        }
+        //--------------Camera Calibration Finish-----------------
+
+        //Start Homography
+
+        image_sub = it_.subscribe("/camera/rgb/image_raw", 1, &OpenCVWebCam::imageCallback, this, image_transport::TransportHints("compressed"));
+
+        laneDetection LaneAlgo();
+
+        //Applying lane detection algorithm
+
+
+    }
+		~OpenCVWebCam() {
+
+		}
+
+
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+    {
+        try
+        {
+            cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+            videoFrame = cv_bridge::toCvShare(msg, "bgr8")->image;
+            undistort(videoFrame, videoFrameUndistorted, cameraMatrix, dist);
+
+            cv::waitKey(30);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+        }
+        warpPerspective(videoFrameUndistorted, videoFrameUndistorted, perspectiveMatrix, videoSize);
+
+        LaneAlgo(videoFrameUndistorted, perspectiveMatrix);
+        LaneAlgo.laneDetctAlgo();
+
+        finalResult = LaneAlgo.getFinalResult();
+
+        //Detect the distance to lane center.
+        laneDistant = LaneAlgo.getLaneCenterDist();
+        if(laneDistant > 0)
+        {
+            ss.str("");
+            ss.clear();
+            ss << abs(laneDistant) << "m " << " To the Right";
+            putText(finalResult, ss.str(), Point(50,50), 0, 2, Scalar(0, 0, 255), 2);
+        }
+        else
+        {
+            ss.str("");
+            ss.clear();
+            ss << abs(laneDistant) << "m " << " To the Left";
+            putText(finalResult, ss.str(), Point(50,50), 0, 2, Scalar(0, 0, 255), 2);
+        }
+
+        imshow("Real Time Execution", finalResult);
+        undistort(videoFrame, videoFrameUndistorted, cameraMatrix, dist);
+        _videoFrameUndistorted = videoFrameUndistorted.clone();
+        LaneAlgo.setInputImage(_videoFrameUndistorted);
+
+
+
+    }
+
+};
+
+int main(int argc, char** argv) {
+	// set up ros
+	ros::init(argc, argv, "opencv_test");
+    ros::NodeHandle nh;
+    cv::namedWindow("view");
+    cv::startWindowThread();
+    image_transport::ImageTransport it(nh);
+
+
+    OpenCVWebCam lanedetection;
+	ROS_INFO("Web");
+
+	ros::spin();
+  	cv::destroyWindow("view");
+	return 0;
+}
